38839e79777b29a6af328c4d69b6dfe53658b408
Nishant Patel <contact@nishant-patel.com>
2017-03-14 13:42:35 -0700

    Final Commit

--- agingpeople/ViewController.swift
+++ agingpeople/ViewController.swift
@@ -9,7 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let people = ["Nishant", "Anu", "Arpit", "Marcus", "Sam", "Parth","Himal"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,20 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
 }
 
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return people.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PersonCell", for: indexPath)
+        cell.textLabel?.text = self.people[indexPath.row]
+        
+        let age = arc4random_uniform(90) + 5
+        cell.detailTextLabel?.text = "\(age) years old"
+        return cell
+    }
+}
